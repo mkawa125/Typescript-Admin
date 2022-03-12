8a43e6c6947e782efef65f6931ac1ff434b43c72
Dahabu Saidi <dahabusaidi@gmail.com>
2022-03-12 08:46:00 +0300

    Remove node_modules from git

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 ### Typescript configurations
 
-`code`
+`
+`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello 3")
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+
+/** Create express app */
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"]
+}));
+
+/** Create our first route */
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World");
+})
+
+/** Listen to specific port */
+app.listen(5000, () => {
+    console.log("Listening to port 5000")
+})
