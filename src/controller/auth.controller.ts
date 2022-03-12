04f6ba00a00b67487165c4c790055234445773eb
Dahabu Saidi <dahabusaidi@gmail.com>
2022-03-12 12:06:34 +0300

    Add register request

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express"
+import { RegisterValidation } from "./validation/register.validation";
 
 export const Register = (req: Request, res: Response) => {
+
+    const body = req.body;
+
+    /** Validate the request inputs */
+    const {error} = RegisterValidation.validate(body);
+
+    if (error) {
+        return res.status(400).send(error.details)
+    }
+
+    /** Check if the password match */
+    if (body.password !== body.password_confirm) {
+        return res.status(400).send({
+            message: "Password does not match"
+        });
+    }
     res.send(req.body);
+
 }
