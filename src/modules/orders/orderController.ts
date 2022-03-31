08712a50dfa8511ffa99a3119cda6a49352f405e
Dahabu Saidi <dahabusaidi@gmail.com>
2022-03-31 09:55:00 +0300

    Export csv

--- src/modules/orders/orderController.ts
+++ src/modules/orders/orderController.ts
@@ -1,5 +1,9 @@
 import { getAllOrders } from './orderService';
 import { Request, Response } from "express"
+import { Parser } from 'json2csv';
+import { getManager } from 'typeorm';
+import { Order } from '../../entity/orderEntity';
+import { OrderItem } from '../../entity/orderItemEntity';
 
 export const Orders = async (req:Request , res:Response) => {
     
@@ -23,8 +27,45 @@ export const Orders = async (req:Request , res:Response) => {
 
 export const ExportCsv = async (req:Request, res:Response) => {
     try {
+        const parser = new Parser({
+            fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']
+        });
+        const repository = getManager().getRepository(Order);
+        const orders =  await repository.find({relations: ['order_items']});
+        const json = [];
+
+        orders.forEach((order: Order)  => {
+            json.push({
+                ID: order.id,
+                Name: order.name,
+                Email: order.email,
+                'Product Title': '',
+                Price: '',
+                Quantity: '',
+            });
+
+            order.order_items.forEach((item: OrderItem) => {
+                json.push({
+                    ID: '',
+                    Name: '',
+                    Email: '',
+                    'Product Title': item.product_title,
+                    Price: item.price,
+                    Quantity: item.quantity,
+                });
+            });
+        });
+
+        const csv = parser.parse(json);
+        res.header('Content-Type', 'text/json');
+        res.attachment('Orders.csv');
+        res.send(csv)
         
     } catch (error) {
-        
+        return res.status(500).json({
+            userMessage: 'Something went wrong, contact the system admin',
+            developerMessage: error.message,
+            success: false
+        });
     }
 }
--- src/modules/orders/orderRoutes.ts
+++ src/modules/orders/orderRoutes.ts
@@ -4,7 +4,7 @@ import { AuthMiddleware } from "../../middleware/auth.middleware";
 
 export const orderRoutes = (router: Router) => {
     router.get('/api/orders', AuthMiddleware, Orders);
-    router.get('/api/orders/export', AuthMiddleware, ExportCsv);
+    router.post('/api/orders/export', AuthMiddleware, ExportCsv);
     // router.post('/api/orders/create', AuthMiddleware, CreateProduct);
     // router.get('/api/orders/:id', AuthMiddleware, GetProduct);
     // router.put('/api/orders/:id', AuthMiddleware, UpdateProduct);
