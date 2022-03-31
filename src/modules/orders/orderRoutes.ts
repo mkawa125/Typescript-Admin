d90c5cd88e3143376d286f849410da1a96fbd3a1
Dahabu Saidi <dahabusaidi@gmail.com>
2022-03-31 10:05:46 +0300

    Code refactor

--- src/modules/orders/orderRoutes.ts
+++ src/modules/orders/orderRoutes.ts
@@ -1,13 +1,9 @@
 import { Orders, ExportCsv } from './orderController';
-import express, { Router } from "express";
+import { Router } from "express";
 import { AuthMiddleware } from "../../middleware/auth.middleware";
 
 export const orderRoutes = (router: Router) => {
     router.get('/api/orders', AuthMiddleware, Orders);
     router.post('/api/orders/export', AuthMiddleware, ExportCsv);
-    // router.post('/api/orders/create', AuthMiddleware, CreateProduct);
-    // router.get('/api/orders/:id', AuthMiddleware, GetProduct);
-    // router.put('/api/orders/:id', AuthMiddleware, UpdateProduct);
-    // router.delete('/api/orders/:id', AuthMiddleware, DeleteProduct);
 
 }
