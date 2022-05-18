9eddbf4c5849e371fb4cc9575a009180f35af777
Dahabu Saidi <dahabusaidi@gmail.com>
2022-05-18 16:45:19 +0300

    Reduce products pagination

--- src/modules/products/productService.ts
+++ src/modules/products/productService.ts
@@ -3,7 +3,7 @@ import { Product } from '../../entity/productEntity';
 
 export const getAllProducts = async (page:any) => {
 
-    const take = 15;
+    const take = 6;
 
     const repository = getManager().getRepository(Product)
     const [data, total] =  await repository.findAndCount({
