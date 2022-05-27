5922a308075af977e08d3256465faaeabb5fd426
Dahabu Saidi <dahabusaidi@gmail.com>
2022-05-27 19:38:33 +0300

    Add bd configurations to .env

---
+++ ormconfig.ts
@@ -0,0 +1,13 @@
+export = {
+        host: process.env.DB_HOST,
+        type: process.env.DB_CONNECTION,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [
+            "src/entity/*.ts"
+        ],
+        logging: false,
+        synchronize: true
+}
